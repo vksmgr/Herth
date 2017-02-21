ebec73780cc118c2c2a4e4cd478a86bde86b7ac9
vksmgr <cseghrce@gmail.com>
2017-02-21 16:54:26 +0530

    MaxMin

--- src/HE_2102/he_run.c
+++ src/HE_2102/he_run.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "he_run.h"
+int max_min[2];
 
 void HE_2102_RUN(){
     min_max();
@@ -20,23 +21,35 @@ void HE_2102_RUN(){
 
 //find max min
 int* f_max_min(int* array, int len){
-    int max_min[2];
-    max_min[0] = max_min[1] = array[0];
-    for (int i = 0; i < len; ++i) {
-        if ( max_min[0] < array[i]) max_min[0] = array[i];
-        else if (max_min[1] > array[i]) max_min[1] = array[i];
+    int max = 0, min = 1;
+    max_min[max] = max_min[min] = array[0];
+    for (int i = 0; i < len; i++) {
+        if ( max_min[max] < array[i]) max_min[max] = array[i];
+        if ( max_min[min] > array[i]) max_min[min] = array[i];
     }
     return &max_min;
 }
 
+
 int min_max(){
-    int N, array[100], *max_min;
+    int N, array[100], *max_min, yes = 0, no = 0;
     scanf("%d",&N);
     for (int i = 0; i < N; ++i) { scanf("%d", &array[i]);}
     max_min = f_max_min(array, N);
-    printf("Max : %d, Min : %d\n",*max_min, *max_min+1);
+    for (int j = 0; j <= max_min[0]; j++) {
+        if(search(array[j], array, N)) yes = 1;
+        else no = 1;
+    }
+    if (no == 1 )printf("NO");
+    if ( yes == 1)printf("YES");
+    //printf("Max : %d, Min : %d\n",*max_min, *(max_min + 1));
+}
 
-
+int search(int elem, int* array, int len){
+    for (int i = 0; i < len; i++) {
+        if(elem == array[i])return 1;
+    }
+    return 0;
 }
 
 
